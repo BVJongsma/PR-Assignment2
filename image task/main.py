017a1037a27ccb03649c97355a8325a4427422c2
SKMuller <kennethmuller@hotmail.com>
2022-01-18 23:03:35 +0100

    start for image loader, now creates folder for extracted images, also beginnings of SIFT

--- image task/main.py
+++ image task/main.py
@@ -1,6 +1,6 @@
 from pipeline import image_loader
 from pipeline import feature_extraction
-from pipeline import classification
+#from pipeline import classification
 from pipeline import clustering
 from pipeline import grid_search
 from sklearn.model_selection import train_test_split
@@ -11,7 +11,7 @@ if __name__ == '__main__':
 
     images = image_loader.load_images(image_path) #input: image path, output: images
     #preprocessing(images) zodra we bij de Data augmentation stap zijn
-    reduced_data = feature_extraction(images) #extract features from images
-    model1, model2 = classification(reduced_data, images) #classify features
-    clustering(model1, model2) #analyse outcome results for original and reduced dataset
-    grid_search() #
+    #reduced_data = feature_extraction(images) #extract features from images
+    #model1, model2 = classification(reduced_data, images) #classify features
+    #clustering(model1, model2) #analyse outcome results for original and reduced dataset
+    #grid_search() #
--- image task/pipeline/classification.py
+++ image task/pipeline/classification.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import image_loader as il
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression #regularization applied by default
+from sklearn.linear_model import LogisticRegression  # regularization applied by default
 from sklearn import metrics
 import numpy as np
+
 # https://www.datacamp.com/community/tutorials/k-nearest-neighbor-classification-scikit-learn
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
-#https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
+# https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
 
 """ Code for loading/augmenting images """
 """
@@ -17,25 +18,30 @@ feature_names = ['one', 'two']
 
 """
 
-X_train, X_test, y_train, y_test = train_test_split(images, reduced_data) #doesn't run since images& reduced data aren't in this file
+X_train, X_test, y_train, y_test = train_test_split(images,
+                                                    reduced_data)  # doesn't run since images& reduced data aren't in this file
 
 
 def KNN(data):
     knn = KNeighborsClassifier(n_neighbors=5).fit(X_train, y_train)
     y_pred = knn.predict(X_test)
-    acc_score = metrics.accuracy_score(y_test, y_pred) #used accuracy score here as evaluation method just for the sake of it
+    acc_score = metrics.accuracy_score(y_test,
+                                       y_pred)  # used accuracy score here as evaluation method just for the sake of it
     print("accuracy score: ", acc_score)
 
 
 def logistic_regression(data):
     lr = LogisticRegression().fit(X_train, y_train)
     y_pred = lr.predict(X_test)
-    acc_score = metrics.accuracy_score(y_test, y_pred) #used accuracy score here as evaluation for the hell of it
+    acc_score = metrics.accuracy_score(y_test, y_pred)  # used accuracy score here as evaluation for the hell of it
     print("logistic regression")
 
-def third_classification():
 
-""""
+def third_classification():
+    return
+
+
+"""
 if __name__ == '__main__':
 
     num = int(input("Enter number. 1. KNN 2.Logistic Regression:"))
