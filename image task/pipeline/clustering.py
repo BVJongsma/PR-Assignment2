a6e22b0d6436e36b890cff58be3888f610d41ba8
BVJongsma <b.v.jongsma@student.rug.nl>
2022-01-21 17:40:36 +0100

    added silhouette coefficient evaluation to clustering DBSCAN

--- image task/pipeline/clustering.py
+++ image task/pipeline/clustering.py
@@ -1,23 +1,26 @@
 from sklearn.cluster import DBSCAN
 from sklearn import preprocessing
 from sklearn.metrics import silhouette_score
-from sklearn.metrics import silhouette_samples
-
-import pandas as pd
 
 
 def myDBSCAN(x_data):
     le = preprocessing.LabelEncoder()
-    labels = le.fit_transform(y_data)
-    clustering = DBSCAN(eps=0.6, min_samples=2).fit(x_data)  # eps???
-    labels_DBSCAN = le.fit_transform(clustering.labels_) #no idea what this actually does
-    return clustering, labels_DBSCAN
+    clustering = DBSCAN(eps=0.6, min_samples=2).fit(x_data)  # adjust eps and min_samples if necessary for our actual task
+    labels = le.fit_transform(clustering.labels_)
 
-#to test out DBSCAN
+    #evaluation
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    try:
+        print("Silhouette Coefficient: %0.3f", silhouette_score(x_data, labels))
+    except:
+        print("An exception occured.")
+    return clustering, labels
+
+
+# to test out DBSCAN
 if __name__ == '__main__':
     x = [[0, 0], [1, 0], [1, 0], [0, 1], [0, 1], [0, 0.5]]
-    #xdf = pd.DataFrame(x)
-    #y = ['a', 'b', 'b', 'c', 'd', 'd', 'd']
     c, lab = myDBSCAN(x)
     x['labels'] = lab
     print("this is c: ", c)
