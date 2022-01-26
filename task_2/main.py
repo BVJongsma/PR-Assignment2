a62521c7c3639053fc0b2a91cb86f0419f664c1f
BVJongsma <b.v.jongsma@student.rug.nl>
2022-01-26 18:24:22 +0100

    task 2 code comments and printstatements removed

--- task_2/main.py
+++ task_2/main.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.semi_supervised import LabelPropagation
 
-# when running, get the error: numpy.core._exceptions._ArrayMemoryError:
-#               Unable to allocate 34.8 GiB for an array with shape (68353, 68353) and data type float64
-
 n = 100
 
 
@@ -22,6 +19,7 @@ def plot_acc_f1(data1, data2, title, plot_title):
     plt.plot(range(1, n + 1), data2)
     plt.xlabel('iteration')
     plt.ylabel('f1-score score')
+
     plt.show()
     plt.savefig(plot_title)
     return
@@ -30,52 +28,41 @@ def plot_acc_f1(data1, data2, title, plot_title):
 if __name__ == '__main__':
     path = "creditcard.csv"
     data = pd.read_csv(path)
-    print("the shape of data is ", data.shape)
-    # data = data[0:1000] #subset to see if code works without taking hours
 
     data_y = data.Class
     data_x = data.iloc[:, range(2, 29)]  # The time and amount columns are not included
-    print("The shape of x is ", data_x.shape, " and the shape of y is ", data_y.shape)
     rus = RandomUnderSampler(sampling_strategy=0.05)
     data_x, data_y = rus.fit_resample(data_x, data_y)
-    print("The new shape of x is ", data_x.shape, " and the new shape of y is ", data_y.shape)
 
     acc_score_2, f1score_2 = [0.0] * n, [0] * n
     acc_score_3, f1score_3 = [0.0] * n, [0] * n
     acc_score_4, f1score_4 = [0.0] * n, [0] * n
 
-    # 1.prepare/split the data
     for i in range(n):
+        # 1.prepare/split the data
         x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.2, stratify=data_y)
         x30_lab, x70_unlab, y30_lab, y70_unlab = train_test_split(x_train, y_train, test_size=0.7,
                                                                   stratify=y_train)  # forget y70_unlab
 
-        # data_labeled = x30_lab.append(y30_lab)
         data_labeled = x30_lab.copy()
         data_labeled['class'] = y30_lab.values
 
         # 2.train baseline model, I choose KNN
-
-        knn = KNeighborsClassifier(n_neighbors=5)  # evaluate later
+        knn = KNeighborsClassifier(n_neighbors=5)
         knn.fit(x30_lab, y30_lab.values)
         y_pred = knn.predict(x_test)
         acc_score_2[i] = accuracy_score(y_test, y_pred)
         f1score_2[i] = f1_score(y_test, y_pred)
-        print("knn:", i, "acc score is", acc_score_2[i])
 
         # 3.train semi-supervised: labeled #
         y70_noise = [-1] * len(y70_unlab)
-
         x_mixed = pd.concat([x30_lab, x70_unlab])
         y_mixed = list(y30_lab.values) + y70_noise
 
-        print("beep")
         semis = LabelPropagation(kernel='knn').fit(x_mixed, y_mixed)
-        print("boop")
         y_pred = semis.predict(x_test)
         acc_score_3[i] = accuracy_score(y_test, y_pred)
         f1score_3[i] = f1_score(y_test, y_pred)
-        print("bwap")
 
         # 4.train baseline model complete dataset
         y_semi = semis.transduction_
@@ -83,10 +70,8 @@ if __name__ == '__main__':
         y_pred = knn_comp.predict(x_test)
         acc_score_4[i] = accuracy_score(y_test, y_pred)
         f1score_4[i] = f1_score(y_test, y_pred)
-        print("end of run", i)
 
     plot_acc_f1(acc_score_2, f1score_2, "step 2: KNN supervised", "step2.png")
-
     print("The mean of accuracy score for supervised baseline model is ", sum(acc_score_2) / n)
     print("The mean of F1 score for supervised baseline model is ", sum(f1score_2) / n)
 
